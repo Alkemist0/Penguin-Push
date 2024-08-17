37a2d33730a92359b9d7f29f3e1ba7488d8a1c4e
Alkemist <50426046+Alkemist0@users.noreply.github.com>
2024-08-17 10:03:52 -0700

    Improve movement
    Added animation, camera follow, easing, and sprite flipping
    
    Co-Authored-By: jageyy <178634358+jageyy@users.noreply.github.com>

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,13 +1,15 @@
 extends CharacterBody2D
 
-const gravity_modifier = 2
-const speed = 500
-const jump_velocity = -1000.0
-
+@export var gravity_modifier = 2.0
+@export var speed = 1000.0
+@export var jump_velocity = -1000.0
+@export var slippery = 40.0
+@export var camera : Camera2D
+@export var camera_speed = 30
 
 func _physics_process(delta: float) -> void :
 	# Add the gravity.
-	if(not is_on_floor()) :
+	if(!is_on_floor()) :
 		velocity += get_gravity() * gravity_modifier * delta
 
 	# Handle jump.
@@ -17,8 +19,28 @@ func _physics_process(delta: float) -> void :
 	# Get the input direction and handle the movement/deceleration.
 	var direction := Input.get_axis("Left", "Right")
 	if(direction) :
-		velocity.x = direction * speed
+		velocity.x = move_toward(velocity.x, direction * speed, speed/slippery)
 	else :
-		velocity.x = move_toward(velocity.x, 0, speed)
-
+		velocity.x = move_toward(velocity.x, 0, speed/slippery)
+	
+	#Flip sprite if facing other direction
+	if(direction > 0) :
+		$Sprite2D.flip_h = false
+	if(direction < 0) :
+		$Sprite2D.flip_h = true
+	
+	#Move camera to be in front of player
+	if($Sprite2D.flip_h) :
+		camera.position.x = move_toward(camera.position.x, position.x - 480, camera_speed)
+	else :
+		camera.position.x = move_toward(camera.position.x, position.x + 480, camera_speed)
+	
+	if(velocity.x == 0) :
+		$Sprite2D/AnimationPlayer.play("Idle")
+	else :
+		$Sprite2D/AnimationPlayer.play("Run")
+		$Sprite2D/AnimationPlayer.speed_scale = velocity.x / speed
+	if(!is_on_floor()) :
+		$Sprite2D/AnimationPlayer.play("Jump")
+	
 	move_and_slide()
