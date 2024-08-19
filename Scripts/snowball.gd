0006959cfaf4ef66ac6d794de06737687413c843
Alkemist <50426046+Alkemist0@users.noreply.github.com>
2024-08-19 00:59:43 -0700

    Finish springs
    Also improved snowball movement

--- Scripts/snowball.gd
+++ Scripts/snowball.gd
@@ -1,27 +1,31 @@
 extends RigidBody2D
 
-@export var push_force = 500.0
+@export var push_force = 15.0
 var size = 1.0
 
 func _ready() -> void:
 	change_size(1)
 
-func _process(_delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	$PushLeft.global_rotation_degrees = 0
 	$PushRight.global_rotation_degrees = 0
 	
 	if($PushLeft.has_overlapping_bodies()) :
-		linear_velocity.x = -push_force / size
+		angular_velocity= -push_force / size
 	if($PushRight.has_overlapping_bodies()) :
-		linear_velocity.x = push_force / size
+		angular_velocity = push_force / size
 		
 	$AnimatableBody2D.global_position = global_position
 	$EnemyDetector.global_position = global_position
 
 func change_size(new_size : int) :
 	if(new_size > 4) : return
+	if(new_size < 1) : 
+		queue_free()
+		return
 	$AnimationPlayer.play("Size" + str(new_size))
 	size = new_size
+	mass = size * 100
 
 func _on_enemy_detector_body_entered(body: Node2D) -> void:
 	if(body.is_in_group("Snowball_Enemy")) :
---
+++ Scripts/spring.gd
@@ -0,0 +1,13 @@
+extends StaticBody2D
+
+@export var launch_force = -1000
+func _ready() -> void:
+	pass 
+
+func _process(_delta: float) -> void:
+	pass
+
+func _on_snowball_detector_area_entered(area: Area2D) -> void:
+	area.get_parent().change_size(area.get_parent().size - 1)
+	area.get_parent().linear_velocity.y = launch_force
+	$AnimationPlayer.play("Open")
