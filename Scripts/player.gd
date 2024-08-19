c3a539c4719e56dfcb93150dac76c8d40a9819f2
Alkemist <50426046+Alkemist0@users.noreply.github.com>
2024-08-18 20:52:23 -0700

    Add coyote time and jump buffering

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,17 +5,29 @@ extends CharacterBody2D
 @export var jump_velocity = -1000.0
 @export var slippery = 40.0
 @export var camera : Camera2D
-@export var camera_speed = 20
+#@export var camera_speed = 20
+var coyote_time = false
+var last_floor = false
+var jump_buffer
 
 func _physics_process(delta: float) -> void :
 	# Add the gravity.
 	if(!is_on_floor()) :
 		velocity += get_gravity() * gravity_modifier * delta
-
+		
 	# Handle jump.
-	if(Input.is_action_just_pressed("Jump") and is_on_floor()) :
+	if(Input.is_action_pressed("Jump")) :
+		if(is_on_floor() || coyote_time) :
+			velocity.y = jump_velocity
+			coyote_time = false
+		else :
+			$JumpBuffer.start()
+			jump_buffer = true
+	
+	if(jump_buffer && is_on_floor()) :
 		velocity.y = jump_velocity
-
+		coyote_time = false
+	
 	# Get the input direction and handle the movement/deceleration.
 	var direction := Input.get_axis("Left", "Right")
 	if(direction) :
@@ -43,12 +55,18 @@ func _physics_process(delta: float) -> void :
 	
 	move_and_slide()
 	
+	if(last_floor && !is_on_floor()) :
+		coyote_time = true
+		$Coyote.start()
+	
+	last_floor = is_on_floor()
+	
 	#ANIMATIONS
 	if(!$FloorChecker.has_overlapping_bodies()) :
 		$Sprite2D/AnimationPlayer.play("Balance")
 	elif($SnowballChecker.has_overlapping_bodies()):
 		$Sprite2D/AnimationPlayer.play("Push")
-		$Sprite2D/AnimationPlayer.speed_scale = velocity.x / speed
+		$Sprite2D/AnimationPlayer.speed_scale = 1
 	else:
 		if(velocity.x != 0) :
 			$Sprite2D/AnimationPlayer.play("Run")
@@ -59,9 +77,9 @@ func _physics_process(delta: float) -> void :
 	if(Input.is_action_pressed("Left") && velocity.x > 0 || Input.is_action_pressed("Right") && velocity.x < 0) :
 		$Sprite2D/AnimationPlayer.play("Turn")
 	
-	if($SnowballChecker.has_overlapping_bodies()) :
-		$Sprite2D/AnimationPlayer.play("Push")
-		$Sprite2D/AnimationPlayer.speed_scale = velocity.x / speed
+	#if($SnowballChecker.has_overlapping_bodies()) :
+		#$Sprite2D/AnimationPlayer.play("Push")
+		#$Sprite2D/AnimationPlayer.speed_scale = velocity.x / speed
 	
 	if(!is_on_floor()) :
 		if(velocity.y < 0) :
@@ -69,5 +87,11 @@ func _physics_process(delta: float) -> void :
 		else :
 			$Sprite2D/AnimationPlayer.play("Fall")
 	
-	if(velocity.x == 0 && !$SnowballChecker.has_overlapping_bodies()) :
+	if(velocity.x == 0) :
 		$Sprite2D/AnimationPlayer.speed_scale = 1
+
+func _on_coyote_timeout() -> void:
+	coyote_time = false
+
+func _on_jump_buffer_timeout() -> void:
+	jump_buffer = false
